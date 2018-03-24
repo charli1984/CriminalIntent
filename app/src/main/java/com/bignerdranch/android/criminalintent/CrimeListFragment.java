21e9d58fbb39bfd3b586f4f5a3af562bbdca7f94
Charli1984 <charli_htp@yahoo.es>
2018-03-24 11:37:47 +0100

    Terminado capitulo 14.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -14,7 +14,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -116,6 +115,7 @@ public class CrimeListFragment extends Fragment {
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         } else {
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyDataSetChanged();
         }
 
@@ -171,7 +171,12 @@ public class CrimeListFragment extends Fragment {
         }
         @Override
         public int getItemCount() {
+
             return mCrimes.size();
+        }
+
+        public void setCrimes(List<Crime> crimes) {
+            mCrimes = crimes;
         }
     }
 
